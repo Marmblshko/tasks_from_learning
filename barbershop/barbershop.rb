cd4e1d306cfa74cb7108f195d09685e03e90d670
Marmblshko <mks316skat@gmail.com>
2023-06-08 17:37:35 +0600

    done 20

--- barbershop/barbershop.rb
+++ barbershop/barbershop.rb
@@ -1,5 +1,38 @@
 require 'sinatra'
 
-get '/' do
+get "/" do
   erb :index
+end
+
+post "/" do
+  @username = params[:username]
+  @phone = params[:phone]
+  @date = params[:date]
+
+  @title = 'thanks'
+  @message = "Dear #{@username}, we'll be waiting for you at #{@date}"
+
+  f = File.open "users.txt", "a"
+  f.write "User: #{@username}, Phone: #{@phone}, Date: #{@date}\n"
+  f.close
+
+  erb :message
+end
+
+get "/admin" do
+  erb :admin
+end
+
+post "/admin" do
+  @admin_pass = params[:admin_pass]
+
+  if @admin_pass == ''
+    @title = 'thanks'
+    @message = send_file 'users.txt'
+    erb :message
+  else
+    @title = 'no'
+    @message = 'no'
+    erb :message
+  end
 end
